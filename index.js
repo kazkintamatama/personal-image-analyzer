85437abeb0a6c494068bbe3eaff817632f885eab
kaz <kazu5230@gmail.com>
2025-03-26 17:02:56 +0900

    addCreate index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import PersonalImageAnalyzer from './PersonalImageAnalyzer';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <PersonalImageAnalyzer />
+  </React.StrictMode>
+);
